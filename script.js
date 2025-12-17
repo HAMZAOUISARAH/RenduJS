d504fbd083796e3f8d861c098849ce7f63933837
HAMZAOUISARAH <SARAH.HAMZAOUI@latoile.em-lyon.com>
2025-12-17 15:35:49 +0100

    OK

--- script.js
+++ script.js
@@ -1,55 +1,54 @@
 const url = "https://js-dynamic-portfolio-data-makerslab-emlyon-cdweb-8f83155c64a0cc.gitlab.io/json/chocolatier.json";
 fetch(url)
- .then(res => res.json())
- .then(data => 
-    {
+    .then(res => res.json())
+    .then(data => {
         // console.log(data);
-        
-   // NAVBAR
-   document.getElementById("logo").textContent = data.nomEntreprise;
-   console.log(data.bouton);
-   
-   document.getElementById("btnNav").textContent = data.bouton;
-   // HEADER
-   document.getElementById("nomEntreprise").textContent = data.nomEntreprise;
-   document.getElementById("slogan").textContent = data.slogan; 
 
-   // BÉNÉFICES CLIENTS
-   const benefices = document.getElementById("benefices");
-   data.listeBeneficesClients.forEach(b => {
-     const li = document.createElement("li");
-     li.textContent = b;
-     benefices.appendChild(li);
-   });
+        // NAVBAR
+        document.getElementById("logo").textContent = data.nomEntreprise;
+        console.log(data.bouton);
 
-   // PRODUITS
-   const produitsContainer = document.getElementById("produitsContainer");
-   data.produits.forEach(p => {
-     const div = document.createElement("div");
-     div.className = "produit";
-     div.innerHTML = `
+        document.getElementById("btnNav").textContent = data.bouton;
+        // HEADER
+        document.getElementById("nomEntreprise").textContent = data.nomEntreprise;
+        document.getElementById("slogan").textContent = data.slogan;
+
+        // BÉNÉFICES CLIENTS
+        const benefices = document.getElementById("benefices");
+        data.listeBeneficesClients.forEach(b => {
+            const li = document.createElement("li");
+            li.textContent = b;
+            benefices.appendChild(li);
+        });
+
+        // PRODUITS
+        const produitsContainer = document.getElementById("produitsContainer");
+        data.produits.forEach(p => {
+            const div = document.createElement("div");
+            div.className = "produit";
+            div.innerHTML = `
 <h3>${p.titre}</h3>
 <p>${p.presentation}</p>
 <img src="${p['image-url']}" alt="${p.titre}">
      `;
-     produitsContainer.appendChild(div);
-   });
+            produitsContainer.appendChild(div);
+        });
 
- // AVIS CLIENTS
-const clients = document.getElementById("clients");
-const aviscontainer=document.getElementById("avis")
-   data.clients.forEach(c => {
-    const div = document.createElement("div");
-    div.className="client";
-    div.innerHTML=`
+        // AVIS CLIENTS
+        const clients = document.getElementById("clients");
+        const aviscontainer = document.getElementById("avis")
+        data.clients.forEach(c => {
+            const div = document.createElement("div");
+            div.className = "client";
+            div.innerHTML = `
     <h4>${c.nom}</h4>
     <p>${c.typePrestation}</p>
     <p>${c.commentaire}</p> `;
 
-    clients.appendChild(div);
+            clients.appendChild(div);
 
 
-   });
-   
-   
-})
+        });
+
+
+    })
